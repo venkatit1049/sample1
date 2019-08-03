8c8d3bf6bb40383c9f1b74b0f692820498cd2814
venkatit1049 <venkatit1049@gmail.com>
2019-08-03 13:13:30 +0530

    added new emp json

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 const controller = {};
 
-controller.list = (req, res) => {
+controller.list = (req, res) => { 
   req.getConnection((err, conn) => {
     conn.query('SELECT * FROM customer', (err, customers) => {
      if (err) {
       res.json(err);
+     } else {
+       
      }
      res.render('customers', {
         data: customers
